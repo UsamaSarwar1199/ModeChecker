cba910b5ed67444cafa008f8efb989376ece9ab6
Usama  Sawar <83979585+UsamaSarwar1199@users.noreply.github.com>
2021-09-20 13:56:06 -0700

    Add files via upload

---
+++ ModeChecker.py
@@ -0,0 +1,17 @@
+print("If you are Sad Enter 1 \n\n\n")
+
+print("if you are Happy Enter 2 \n\n\n\n\n")
+a=int(input())
+
+if a==1:
+    print("Why are you not happy\n\n")
+    print("what is  the reason of your sadness 😢 \n\n ")
+    print("if the reason  is money . Dont worry . You can get it if you workhard.\n\n")
+    print("Relax Buddy 😊")
+    
+if a==2:
+    print("Good You are Happy.😍")
+    print("You are among the very few people who actually know how to be happy.")
+    print("Awesome")
+    print("😎")
+    print("See you soon😊")
